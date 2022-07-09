545f558af7d91f8b513548074d99b2fa2b6ec51d
sidiousvic <sidiousvic@gmail.com>
2022-07-09 14:20:06 +0900

    hide react hook code

--- mod.ts
+++ mod.ts
@@ -1,5 +1,2 @@
 export { motor } from "./motor/index.ts";
 export type { Dict, Keys, MotorSpec } from "./motor/index.d.ts";
-// UNDER CONSTRUCTION //
-// export { useMotor } from "./motor/hooks.tsx";
-// UNDER CONSTRUCTION //
